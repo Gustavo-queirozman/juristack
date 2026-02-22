99f42de5456dffd65624dad6b04a54063fdad454
root <gustavoqueirozmit@gmail.com>
2026-02-22 14:43:57 -0300

    procuração

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DataJudController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DocumentTemplateController;
+use App\Http\Controllers\DocumentController;
 
 // DataJud routes - require authentication
 Route::middleware('auth')->group(function () {
@@ -25,6 +27,44 @@ Route::middleware('auth')->group(function () {
     Route::post('customers/{customer}/files', [CustomerController::class, 'uploadForCustomer'])->name('customers.files.store');
     Route::get('customers/{customer}/files/{file}/download', [CustomerController::class, 'downloadFile'])->name('customers.files.download');
     Route::delete('customers/{customer}/files/{file}', [CustomerController::class, 'destroyFile'])->name('customers.files.destroy');
+});
+
+
+
+Route::prefix('document-templates')->group(function () {
+    Route::get('/', [DocumentTemplateController::class, 'index']);     // List templates
+    Route::get('/{id}', [DocumentTemplateController::class, 'show']);  // Show single template
+    Route::post('/', [DocumentTemplateController::class, 'store']);    // Create template
+    Route::put('/{id}', [DocumentTemplateController::class, 'update']); // Update template
+    Route::patch('/{id}', [DocumentTemplateController::class, 'update']); // Partial update
+    Route::delete('/{id}', [DocumentTemplateController::class, 'destroy']); // Delete template
+});
+
+
+Route::prefix('documents')->group(function () {
+    // List documents (with filters)
+    Route::get('/', [DocumentController::class, 'listDocuments']);
+    // Show single document
+    Route::get('/{id}', [DocumentController::class, 'showDocument']);
+    // Create document
+    Route::post('/', [DocumentController::class, 'createDocument']);
+    // Update document
+    Route::put('/{id}', [DocumentController::class, 'updateDocument']);
+    Route::patch('/{id}', [DocumentController::class, 'updateDocument']);
+    // Generate document from template
+    Route::post('/{id}/generate', [DocumentController::class, 'generateDocument']);
+    /*
+    |--------------------------------------------------------------------------
+    | Form Routes (Nested Resource Style)
+    |--------------------------------------------------------------------------
+    */
+    // Create/attach form
+    Route::post('/{id}/form', [DocumentController::class, 'createForm']);
+    // Show form
+    Route::get('/{id}/form', [DocumentController::class, 'showForm']);
+    // Update form
+    Route::put('/{id}/form', [DocumentController::class, 'updateForm']);
+    Route::patch('/{id}/form', [DocumentController::class, 'updateForm']);
 });
 
 Route::middleware('auth:customer')->group(function () {
